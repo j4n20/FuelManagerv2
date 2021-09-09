cb46e5038c7922d22b79c5466be85b853e4e1507
j4n20 <jan-gruchott@live.de>
2021-09-09 19:07:44 +0200

    Import View/model

---
+++ FuelClient/ViewModels/ImportViewmodel.cs
@@ -0,0 +1,19 @@
+﻿using FuelClient.Service;
+using FuelClient.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FuelClient.ViewModels
+{
+    class ImportViewmodel : ViewModelBase
+    {
+        public ICommand ImportCommand { get; set; }
+
+       
+    }
+}
