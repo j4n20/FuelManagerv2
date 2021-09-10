e54d7ea4b7f91d19103fa9affdcdd2884d2bb91a
thom-zim <thomaszimmermann71@gmail.com>
2021-09-10 19:26:42 +0200

    zwischenpush

--- FuelClient/App.xaml.cs
+++ FuelClient/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using FuelClient.Controller;
 using Autofac;
 
 namespace FuelClient
@@ -14,5 +16,19 @@ namespace FuelClient
     /// </summary>
     public partial class App : Application
     {
+        public IContainer Container { get; set; }
+        public Service.FEmployee Employee { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).Where(t => t.IsClass && (t.Namespace.Contains("Controller") || t.Namespace.Contains("ViewModels") || t.Namespace.Contains("Views")));
+            containerBuilder.RegisterInstance(this);
+
+            Container = containerBuilder.Build();
+
+            Container.Resolve<LoginWindowController>().Initialize();
+        }
     }
 }
